4c75969d8834a5fea359ef437c47076fc3bf6ac0
fady <fady.tarek.1995@gmail.com>
2024-08-04 22:49:07 +0300

    added migration for user product order tables

---
+++ migrations/sqls/20240804192345-user-table-up.sql
@@ -0,0 +1,7 @@
+/* Replace with your SQL commands */
+CREATE TABLE user (
+    id SERIAL PRIMARY  KEY,
+    firstName VARCHAR(150),
+    lastName VARCHAR(150),
+    password VARCHAR(100),
+);
---
+++ migrations/sqls/20240804192353-order-table-up.sql
@@ -0,0 +1,11 @@
+/* Replace with your SQL commands */
+CREATE TABLE order (
+
+status of order (active or complete)
+    id SERIAL PRIMARY  KEY,
+    productid integer,
+    productquantity integer,
+    user_id integer,
+    orderstatus VARCHAR(150),
+
+);
