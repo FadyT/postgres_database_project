ae338b4da2f104ce6d41c4b9c10394cf97a34acd
fady <fady.tarek.1995@gmail.com>
2024-08-09 19:48:26 +0300

    fixed database

--- src/Model/order.ts
+++ src/Model/order.ts
@@ -60,7 +60,7 @@ export class OrdersList {
       } catch (err) {
         throw new Error(`${err}`)
       }
-  
+
         try {
         const sql = 'INSERT INTO order_products (quantity ,order_id , product_id ) VALUES ($1,$2,$3)  RETURNING *'
         // @ts-ignore
@@ -77,11 +77,11 @@ export class OrdersList {
         
       }
       
-    
+
       async create(b: order): Promise<order> {
           try {
             console.log(`trying to create order with data ${b.productsID}${b.quantity}${b.UserID}${b.status} `)
-        const sql = 'INSERT INTO orders (productID, quantity , UserID, status) VALUES($1, $2 , $3 , $4) RETURNING *'
+        const sql = 'INSERT INTO orders (productid, productquantity , user_id, orderstatus) VALUES($1, $2 , $3 , $4) RETURNING *'
         // @ts-ignore
         const conn = await client.connect()
     
